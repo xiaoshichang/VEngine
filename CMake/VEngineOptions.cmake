e081aa958432df2575925a833090290b4a6bfac3
xiaoshichang <511680777@qq.com>
2026-06-09 23:24:47 +0800

    remove useless files

--- CMake/VEngineOptions.cmake
+++ CMake/VEngineOptions.cmake
@@ -4,7 +4,6 @@ option(VE_BUILD_PLAYER "Build Windows player" ON)
 option(VE_BUILD_EDITOR "Build Windows editor" ON)
 option(VE_BUILD_TESTS "Build Windows tests" ON)
 option(VE_BUILD_TOOLS "Build command line tools" ON)
-option(VE_BUILD_SHADER_TESTS "Build shader pipeline smoke tests" ON)
 option(VE_BUILD_IOS_PLAYER "Build iOS player app" OFF)
 
 option(VE_ENABLE_D3D11 "Enable D3D11 RHI" ON)
